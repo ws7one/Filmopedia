d6f6f42b2473661be13508e899c19a6cf4710482
Philip Viningston <philip.viningston@tech9.com>
2020-08-26 17:55:41 +0530

    Added debounce to textinput

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: 'rallycoding',
   rules: {
-    'react/require-extension': 'off'
+    'react/require-extension': 'off',
+    'no-undef': 'off'
   }
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../redux/actions/home/HomeActions';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            debounce: 500,
+            undebouncedText: '',
+            minSearchTextLength: 3
+        };
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer);
+    }
+
+    changeMessage = (text) => {
+        if (text.length > this.state.minSearchTextLength) {
+            this.props.changeMessage(text);
+        }
+    }
+
+    debouncedChangeText = (text) => {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => this.changeMessage(text), this.state.debounce);
+    }
+
+    handleTextChange = (text) => {
+        this.setState({ undebouncedText: text }, () => this.debouncedChangeText(text));
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>{this.props.home.message}</Text>
+                <TextInput
+                    style={styles.textInputStyle}
+                    onChangeText={text => this.handleTextChange(text)}
+                    onBlur={() => clearTimeout(this.timer)}
+                    value={this.state.undebouncedText}
+                />
             </View>
         );
     }
@@ -21,7 +56,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 10
+    },
+    textInputStyle: {
+        width: '100%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5
     }
 });
 
